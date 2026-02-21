12be97495bd0521f2b3208255077fffeb96d3851
raiko1729 <182466719+raiko1729@users.noreply.github.com>
2026-02-22 07:27:10 +0900

    Renderに合わせるためSERVER URLを変更，

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 const io = new Server(server, {
   cors: { origin: '*' }
 });
